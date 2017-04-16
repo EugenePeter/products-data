10f9e99a87f728141a03fe1c4b8cf8a5cb1a2063
Eugene <eugenedatsmee@gmail.com>
2017-04-16 15:23:21 +1200

    added data.js

---
+++ data.js
@@ -0,0 +1,66 @@
+var data = [
+
+    {
+        id:1,
+        name: "Cat Fuzz",
+        price: 123.12,
+        desc: "Furry Fuzzy screeches",
+        image: "assets/images/saree/1.jpg"
+    },
+    {
+        id:2,
+        name: "Tweedle Echo",
+        price:132.32,
+        desc: "If I've said it once I've said twice...",
+        image: "assets/images/saree/2.jpg"
+    },
+    {
+        id:3,
+        name: "Multiplex Echo",
+        desc:"Two delays, in series or parallel.",
+        price:213.21,
+        image:"assets/images/saree/3.jpg"
+    },
+    {
+        id:4,
+        name: "Shifty thing",
+        desc:"5 and 6 stages of smooth shiftyness.",
+        price:199.99,
+        image:"assets/images/saree/4.jpg"
+    },
+    {
+        id:5,
+        name: "Shifty thing",
+        desc:"5 and 6 stages of smooth shiftyness.",
+        price:199.99,
+        image:"assets/images/saree/5.jpg"
+    },
+    {
+        id:6,
+        name: "Delay thing",
+        desc:"Echoing delay thing with extra knobiness.",
+        price:199.99,
+        image:"assets/images/saree/6.jpg"
+    },
+    {
+        id:7,
+        name: "Furr Face Fuzz",
+        desc:"Furr Face suzz with shag carpet noise.",
+        price:199.99,
+        image:"assets/images/saree/7.jpg"
+    },
+    {
+        id:8,
+        name: "Super Boosty",
+        desc:"Super Boostiness with get louder knob.",
+        price:120.99,
+        image:"assets/images/saree/8.jpg"
+    },
+    //    {
+    //        id:9,
+    //        name: "Car Crash",
+    //        desc:"Car Crash Fuzz, with two transistor fuzz action.",
+    //        price:156.99,
+    //        image:"assets/images/saree/5.jpg"
+    //    }
+];
